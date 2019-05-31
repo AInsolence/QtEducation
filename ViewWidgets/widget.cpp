9279e19322a3e700e33f82dab6649fe8f5fc8cf0
AInsolence <antonpythondev@gmail.com>
2019-05-31 16:28:26 +0600

    added PushButtons, Checkbox, RadioButtons, GroupBox examples

--- ViewWidgets/widget.cpp
+++ ViewWidgets/widget.cpp
@@ -3,26 +3,28 @@
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
-
-    //QLable example NOTE: windows version only underscored with ALT and doesnt work as reference
-
     QLabel* label = new QLabel("&My name: ", this);
     QLabel* label2 = new QLabel("&My age: ", this);
 
     QLineEdit* lineEditArea = new QLineEdit("Name", this);
+    //QLable example NOTE: windows version only underscored with ALT and doesnt work as reference
     label->setBuddy(lineEditArea);
     QSpinBox* lineEditArea_2 = new QSpinBox(this);
     lineEditArea_2->setRange(0, LONG_MAX);
     label2->setBuddy(lineEditArea_2);
 
+    QHBoxLayout* wrapperLayoutLevel_1 = new QHBoxLayout();
+    wrapperLayoutLevel_1->setMargin(10);
+    wrapperLayoutLevel_1->setSpacing(50);
+    setLayout(wrapperLayoutLevel_1);
+
     QVBoxLayout* labelExamplelayout = new QVBoxLayout();
+    wrapperLayoutLevel_1->addLayout(labelExamplelayout);
     labelExamplelayout->addWidget(label);
     labelExamplelayout->addWidget(lineEditArea);
     labelExamplelayout->addWidget(label2);
     labelExamplelayout->addWidget(lineEditArea_2);
     labelExamplelayout->setSpacing(20);
-    setLayout(labelExamplelayout);
-    show();
 
     //QProgressBar example
     QHBoxLayout* progressLayout = new QHBoxLayout();
@@ -50,6 +52,67 @@ Widget::Widget(QWidget *parent)
     connect(lineEditArea_2, qOverload<int>(&QSpinBox::valueChanged), displayLCD, qOverload<int>(&QLCDNumber::display));
 
     labelExamplelayout->addWidget(displayLCD);
+
+    //Buttons examples
+    QVBoxLayout* buttonsExamplelayout = new QVBoxLayout();
+    buttonsExamplelayout->setAlignment(Qt::AlignCenter);
+    buttonsExamplelayout->setMargin(10);
+    buttonsExamplelayout->setSpacing(20);
+    wrapperLayoutLevel_1->addLayout(buttonsExamplelayout);
+
+    QPushButton* normalButton = new QPushButton("&Normal button");
+    QPushButton* toggledButton = new QPushButton("Toggled button");
+    toggledButton->setCheckable(true);//make toggle button
+    toggledButton->setChecked(true);//make "on" by default
+    QPushButton* flatButton = new QPushButton("Flat button");
+    flatButton->setFlat(true);
+    QPushButton* pixmapButton = new QPushButton("Pixmap button");
+    QPixmap trooper(":/images/calculator.png");
+    pixmapButton->setIcon(trooper);
+    pixmapButton->setIconSize(QSize(30, 30));
+    normalButton->setDefault(true);
+
+    buttonsExamplelayout->addWidget(normalButton);
+    buttonsExamplelayout->addWidget(toggledButton);
+    buttonsExamplelayout->addWidget(flatButton);
+    buttonsExamplelayout->addWidget(pixmapButton);
+
+    QMenu* dropDownMenu = new QMenu (pixmapButton);
+    dropDownMenu->addAction("Push me first");
+    dropDownMenu->addAction("Push me second");
+    dropDownMenu->addAction("Exit", &QApplication::quit);
+    pixmapButton->setMenu(dropDownMenu);
+
+    //Checkbox examples
+    QCheckBox* normalCheckBox = new QCheckBox("&Filled segments");
+    QCheckBox* tristateCheckbox = new QCheckBox("&Triple state box");
+
+    tristateCheckbox->setTristate(true);//allows using third "undefined" state
+    tristateCheckbox->setCheckState(Qt::PartiallyChecked);
+
+    buttonsExamplelayout->addWidget(normalCheckBox);
+    buttonsExamplelayout->addWidget(tristateCheckbox);
+
+    //Radio buttons and GroupBox example
+    QGroupBox* radioBox = new QGroupBox("&Choose your color:");
+    buttonsExamplelayout->addWidget(radioBox);
+    radioBox->setLayout(new QHBoxLayout());
+
+    QRadioButton* redRadio = new QRadioButton("&Red");// use Alt+R shortcut
+    QRadioButton* blueRadio = new QRadioButton("&Blue");
+    QRadioButton* greenRadio = new QRadioButton("&Green");
+
+    radioBox->layout()->addWidget(redRadio);
+    radioBox->layout()->addWidget(blueRadio);
+    radioBox->layout()->addWidget(greenRadio);
+    redRadio->setChecked(true);
+    blueRadio->setFocus();// set TAB focus on button
+
+    QPushButton* exit = new QPushButton("Exit");
+    radioBox->layout()->addWidget(exit);
+    connect(exit, &QPushButton::clicked, &QApplication::quit);
+
+    show();
 }
 
 Widget::~Widget()
