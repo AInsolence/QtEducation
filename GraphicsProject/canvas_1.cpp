b67bea807dfdbe7fa815a77de5b8ba1252b87a26
AInsolence <antonpythondev@gmail.com>
2019-07-18 19:41:33 +0600

    added primitives painting examples

--- GraphicsProject/canvas_1.cpp
+++ GraphicsProject/canvas_1.cpp
@@ -1,4 +1,6 @@
 #include "canvas_1.h"
+#include <QtMath>
+#include <QVector>
 
 Canvas_1::Canvas_1(QWidget *parent)
     : QWidget(parent)
@@ -11,19 +13,61 @@ Canvas_1::~Canvas_1()
 
 }
 
-void Canvas_1::paintEvent(QPaintEvent *pe)
+void Canvas_1::paintEvent(QPaintEvent *)
 {
     QPainter* painter_1 = new QPainter(this);
 
-    QPen pen_1(QColor(Qt::red).darker(200), 4,
+    QPen pen_1(QColor(Qt::red).darker(200), 3,
                Qt::DashDotLine, Qt::RoundCap, Qt::MiterJoin);
 
     QPixmap brushImage(":/image/woman.jpg");
-    QBrush brush_1(brushImage);
+    // QBrush brush_1(Qt::red, Qt::HorPattern);
+    QBrush brush_1(brushImage);// use as texture pattern
     // settings
     painter_1->setPen(pen_1);
     painter_1->setBrush(brush_1);
 
-    painter_1->drawEllipse(0,0,this->width(),this->width()/2);
+    painter_1->drawEllipse(0,0,this->width()/2,this->width()/4);
+
+    // paint primitives examples
+    // drawPoint
+    const int numOfPoints = 12;
+    double startCoordX = 110.0;
+    double startCoordY = 300.0;
+    double radius = 100.0;
+
+    QPointF points [numOfPoints];
+
+    for(int i = 0; i < numOfPoints; ++i){
+        double angle = 2 * M_PI * i/numOfPoints;
+        double pointX = startCoordX + cos(angle)*radius;
+        double pointY = startCoordY + sin(angle)*radius;
+        painter_1->drawPoint(QPointF(pointX, pointY));
+        points[i] = QPointF(pointX, pointY);
+    }
+    // drawPolyline
+    painter_1->drawPolyline(points, numOfPoints/2);
+    // drawConvexPolygon
+    painter_1->drawConvexPolygon(points, 5);// may be faster but only for convex
+    // draw rectangles without border
+    QConicalGradient gradient(150, 150, 90);
+    gradient.setColorAt(0, Qt::green);
+    gradient.setColorAt(0.4, Qt::blue);
+    gradient.setColorAt(0.8, Qt::red);
+    gradient.setColorAt(1, Qt::green);
+    QBrush myBrush(gradient);
+
+    painter_1->fillRect(QRect(100, 100, 100, 100), myBrush);
+    painter_1->eraseRect(QRect(50, 50, 40, 80));
+    // draw simple rectangle
+    painter_1->drawRect(300, 100, 100, 200);
+    painter_1->drawRoundedRect(QRect(400, 200, 200, 150), 20, 100);
+    // draw circle
+    painter_1->drawEllipse(400, 50, 50, 50);
+    // draw chord and pie
+    painter_1->drawChord(500, 50, 200, 200,
+                         90*16, 90*16);// connect two angles on ellips
+    painter_1->drawPie(600, 50, 200, 200,
+                       30*16, 330*16);// pie between two angles
 
 }
--- GraphicsProject/main.cpp
+++ GraphicsProject/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Canvas_1 w;
+    w.setMinimumWidth(800);
     w.show();
 
     return a.exec();
