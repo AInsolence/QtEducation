3c89f22b7f35a9f5aabd61e54340785272b9e293
AInsolence <antonpythondev@gmail.com>
2019-11-15 16:30:32 +0600

    added qss example

---
+++ QSS_example/qsswidget.cpp
@@ -0,0 +1,39 @@
+#include "qsswidget.h"
+
+#include "QPushButton"
+#include "QLayout"
+#include "QFile"
+#include "QDebug"
+
+QSSWidget::QSSWidget(QWidget *parent)
+    : QWidget(parent)
+{
+    QVBoxLayout* layout = new QVBoxLayout;
+
+    QPushButton* button_1 = new QPushButton("Button 1");
+    QPushButton* button_2 = new QPushButton("Button 2");
+    QPushButton* button_3 = new QPushButton("Button 3");
+
+    layout->addWidget(button_1);
+    layout->addWidget(button_2);
+    layout->addWidget(button_3);
+
+    setLayout(layout);
+    resize(500, 300);
+
+    QFile styleFile(":/style.qss");
+    if(styleFile.open(QFile::ReadOnly)){
+        QString style (styleFile.readAll());
+        setStyleSheet(style);
+    }
+    else {
+        qDebug() << "Cannot open style.qss file";
+    }
+
+    ensurePolished();
+}
+
+QSSWidget::~QSSWidget()
+{
+
+}
---
+++ QSS_example/qsswidget.h
@@ -0,0 +1,15 @@
+#ifndef QSSWIDGET_H
+#define QSSWIDGET_H
+
+#include <QWidget>
+
+class QSSWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    QSSWidget(QWidget *parent = 0);
+    ~QSSWidget();
+};
+
+#endif // QSSWIDGET_H
